4fe220b2939909331ae5c71d06050684560b9660
Anjali <anjalinegian2002@gmail.com>
2023-09-18 17:43:11 +0530

    Install all dependancies in react for testing. react testing library and jest, and successfully run my first test

---
+++ src/__ tests __/App.test.tsx
@@ -0,0 +1,12 @@
+// src/__ tests __/App.test.tsx
+
+import '@testing-library/jest-dom'
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+
+
+test("Renders the main page", () => {
+    render(<App />)
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+})
